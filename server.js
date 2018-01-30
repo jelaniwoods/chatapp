3cafeb4a7eed695ad57c37901690ab0a6c191df3
baby snaps <jmw686@nyu.edu>
2018-01-30 16:32:14 -0600

    finish server

---
+++ server.js
@@ -0,0 +1,66 @@
+const mongo = require('mongodb').MongoClient;
+const io = require('socket.io').listen(3400).sockets;
+
+
+
+//connection to mongo
+mongo.connect('mongodb://localhost/mongo_chat', (err, db) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log('connection ongoing~');
+
+  //socket.io
+  io.on('connection', () => {
+    let chat = db.collection('chats');
+
+
+    // create function to send status
+    sendStatus = (s) => {
+      socket.emit('status', s);
+    }
+
+
+    //get chats from mongo
+
+    chat.find().limit(100).sort({_id:1}).toArray(function(err, res) {
+      if (err) {
+        throw err;
+      }
+      //emit messages
+      socket.emit('output', res);
+    });
+
+
+    socket.on('input', (data) => {
+      let name = data.name;
+      let message = data.message;
+
+      // check for name and message
+      if (name === '' || message === '') {
+        sendStatus('please enter name and message');
+      }
+      else {
+        // add to db
+        chat.insert({name: name, message: message}, () => {
+          io.emit('output', [data]);
+
+          sendStatus({
+            message: 'Message sent',
+            clear: true
+          });
+        });
+      }
+    });
+
+
+    // handle clear
+    socket.on('clear', function (data) {
+      //remove all chats
+      chat.remove({}, function () {
+        socket.emit('cleared');
+      });
+    });
+
+  });
+});
